b14387f1f80ef1b9708a77079a855c9f40c5c00e
JoseCapellan11 <jose.a.capellan.h@gmail.com>
2026-08-28 20:44:49 -0400

    Estructura inicial de la solucion en capas: Domain, Application, Infrastructure, Web (MVC) y Api

---
+++ src/GestionTicketsCombustible.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionTicketsCombustible.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
